102bcda9f60dbef31387d36ecb796e63bf21051c
nguyenvanbao1904 <bao19042004@gmail.com>
2023-11-20 19:46:50 +0700

    fix icon

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 const toast = document.querySelector(".toast")
 const btns = document.querySelectorAll('.btn')
 
-const createToast = ({status, icon, title, desc, time})=>{
+const createToast = ({status, title, desc, time})=>{
     const toast_container = document.createElement('div');
     toast_container.classList.add("toast_container");
     toast_container.classList.add(`toast_${status}`)
     toast_container.style.animation = `slideIn var(--timeSlideIn) ease, fadeout calc(var(--timeSlideIn) + ${time+'ms'}) linear`;
+    const icon = {
+        success: `<i class="fa-solid fa-circle-check toast_icon"></i>`,
+        error: `<i class="fa-solid fa-circle-exclamation toast_icon"></i>`
+    }
     const htmls =
     `
         <div class="toast_status"></div>
         <div class="toast_body">
-            ${icon}
+            ${icon[status]}
             <div class="toast_content">
                 <h4 class="toast_title">${title}</h4>
                 <p class="toastDesc">${desc}</p>
@@ -37,7 +41,6 @@ btns.forEach(btn => {
         if(e.target.className.includes("success")){
             createToast({
                 status: "success",
-                icon: '<i class="fa-solid fa-circle-check toast_icon"></i>',
                 title: "Thành Công!",
                 desc: "Bạn đã đăng ký thành công tài khoản tại f8.",
                 time: 3000
@@ -45,7 +48,6 @@ btns.forEach(btn => {
         } else if(e.target.className.includes("error")){
             createToast({
                 status: "error",
-                icon: '<i class="fa-solid fa-circle-check toast_icon"></i>',
                 title: "Thất bại!",
                 desc: "Có lỗi xảy ra, vui lòng liên hệ quản trị viên.",
                 time: 3000
